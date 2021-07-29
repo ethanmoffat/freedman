d861f5cc115e3a750739b1e40e5a4be34909502a
Ethan Moffat <ethan@moffat.io>
2021-07-28 23:37:02 -0700

    Add meme converters

---
+++ Converters/Meme/BathroomConverter.cs
@@ -0,0 +1,33 @@
+﻿using AutomaticTypeMapper;
+using freedman.Unit;
+using System;
+using System.Threading.Tasks;
+
+namespace freedman.Converters.Meme
+{
+    [AutoMappedType]
+    public class BathroomConverter : IUnitConverter
+    {
+        public IUnit DefaultTarget => new GenericUnit(0, "washroom");
+
+        public bool IsConverterFor(IUnit unit)
+        {
+            return string.Equals(unit.Units, "bathroom", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public Task<IUnit> FromSIUnitAsync(IUnit source)
+        {
+            return Task.FromResult(source);
+        }
+
+        public Task<IUnit> ToSIUnitAsync(IUnit source)
+        {
+            return Task.FromResult(source);
+        }
+
+        public IUnit UnitFactory(double value, string units)
+        {
+            return new GenericUnit(value, units);
+        }
+    }
+}
---
+++ Converters/Meme/WashroomConverter.cs
@@ -0,0 +1,33 @@
+﻿using AutomaticTypeMapper;
+using freedman.Unit;
+using System;
+using System.Threading.Tasks;
+
+namespace freedman.Converters.Meme
+{
+    [AutoMappedType]
+    public class WashroomConverter : IUnitConverter
+    {
+        public IUnit DefaultTarget => new GenericUnit(0, "bathroom");
+
+        public bool IsConverterFor(IUnit unit)
+        {
+            return string.Equals(unit.Units, "washroom", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public Task<IUnit> FromSIUnitAsync(IUnit source)
+        {
+            return Task.FromResult(UnitFactory(source.Value, "washroom"));
+        }
+
+        public Task<IUnit> ToSIUnitAsync(IUnit source)
+        {
+            return Task.FromResult(UnitFactory(source.Value, "bathroom"));
+        }
+
+        public IUnit UnitFactory(double value, string units)
+        {
+            return new GenericUnit(value, units);
+        }
+    }
+}
